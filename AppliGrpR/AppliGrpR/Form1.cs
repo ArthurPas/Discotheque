0c02f9fb1de42e781a079d9d303eca1c5a916a8b
Aïssa KAHLOUL <aissageiffel@outlook.com>
2021-06-04 14:19:08 +0200

    US5 ( sans affichage avec listbox )

--- AppliGrpR/AppliGrpR/Form1.cs
+++ AppliGrpR/AppliGrpR/Form1.cs
@@ -19,6 +19,7 @@ namespace AppliGrpR
         {
             InitializeComponent();
             InitConnexion();
+            ListRetard10J();
         }
         public void InitConnexion()
         {
@@ -84,6 +85,21 @@ namespace AppliGrpR
 
             ConsultAlbum();
             SearchName.Text = "";
+        }
+        public void ListRetard10J()
+        {
+            string sql = "SELECT DISTINCT NOM_ABONNÉ,PRÉNOM_ABONNÉ from ABONNÉS" +
+                " INNER JOIN EMPRUNTER on ABONNÉS.CODE_ABONNÉ = EMPRUNTER.CODE_ABONNÉ " +
+                "WHERE DATEDIFF(day, Emprunter.DATE_RETOUR_ATTENDUE, GETDATE()) > 10;";
+            OleDbCommand cmd = new OleDbCommand(sql, dbCon);
+            cmd.ExecuteNonQuery();
+            OleDbDataReader reader = cmd.ExecuteReader();
+            while (reader.Read())
+            {
+                string nom = reader.GetString(0);
+                string prenom = reader.GetString(1);
+                Console.WriteLine(nom+prenom);
+            }
         }
     }
 }

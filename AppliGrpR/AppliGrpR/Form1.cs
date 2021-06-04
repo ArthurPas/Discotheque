4ac0c44dc9310152262a2846657f6d48ded0f691
Arthur Pascal <arthur.pascal@etu.u-bordeaux.fr>
2021-06-04 10:57:36 +0200

    US1 fonctionnelle

---
+++ AppliGrpR/AppliGrpR/Form1.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.OleDb;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace AppliGrpR
+{
+    public partial class Form1 : Form
+    {
+        OleDbConnection dbCon;
+        public Form1()
+        {
+            InitializeComponent();
+            InitConnexion();
+        }
+        public void InitConnexion()
+        {
+
+            string ChaineBd = "Provider=SQLOLEDB;Data Source=INFO-DORMEUR;Initial Catalog=MusiquePT2_R;Integrated Security=SSPI;";
+
+            dbCon = new OleDbConnection(ChaineBd);
+            dbCon.Open();
+        }
+
+        public void AddAbonnes()
+        {
+            string insert = "insert into ABONNÉS(CODE_PAYS,NOM_ABONNÉ,PRÉNOM_ABONNÉ,LOGIN_ABONNÉ,PASSWORD_ABONNÉ) values(?,?,?,?,?)";
+            OleDbCommand cmd = new OleDbCommand(insert, dbCon);
+            cmd.Parameters.Add("CODE_PAYS", OleDbType.Integer).Value = textBox1.Text;
+            cmd.Parameters.Add("NOM_ABONNÉ", OleDbType.VarChar).Value = textBox2.Text;
+            cmd.Parameters.Add("PRÉNOM_ABONNÉ", OleDbType.VarChar).Value = textBox3.Text;
+            cmd.Parameters.Add("LOGIN_ABONNÉ", OleDbType.VarChar).Value = textBox4.Text;
+            cmd.Parameters.Add("PASSWORD_ABONNÉ", OleDbType.VarChar).Value = textBox5.Text;
+            cmd.ExecuteNonQuery();
+        }
+
+        private void AddButton_Click(object sender, EventArgs e)
+        {
+            AddAbonnes();
+            textBox1.Text = "";
+            textBox2.Text = "";
+            textBox3.Text = "";
+            textBox4.Text = "";
+            textBox5.Text = "";
+        }
+    }
+}

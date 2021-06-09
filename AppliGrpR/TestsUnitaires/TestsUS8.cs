b59e9ca04905c62dfd343c733a2759cb01dc12b4
Arthur Pascal <arthur.pascal@etu.u-bordeaux.fr>
2021-06-09 13:57:52 +0200

    tests US 8

---
+++ AppliGrpR/TestsUnitaires/TestsUS8.cs
@@ -0,0 +1,60 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.OleDb;
+using System.Diagnostics;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using AppliGrpR;
+namespace TestsUnitaires
+{
+    [TestClass]
+    public class TestsUS8
+    {
+        OleDbConnection dbCon;
+        string ChaineBd = "Provider=SQLOLEDB;Data Source=INFO-DORMEUR;Initial Catalog=MusiquePT2_R;Integrated Security=SSPI;";
+        Accueil accueil = new Accueil();
+        AdministrateurAccueil adminAcc = new AdministrateurAccueil();
+        List<Albums> topTest = new List<Albums>();
+        public void InitConnexion()
+        {
+            dbCon = new OleDbConnection(ChaineBd);
+            dbCon.Open();
+        }
+        [TestMethod]
+        public void TestsPlusDunAn()
+        {
+            InitConnexion();
+            bool same = true;
+            List<int> test = new List<int>();
+            string request = "SELECT * " +
+                "FROM ALBUMS " +
+                "FULL JOIN EMPRUNTER ON ALBUMS.CODE_ALBUM = EMPRUNTER.CODE_ALBUM " +
+                "WHERE DATEDIFF(day, DATE_EMPRUNT,GETDATE()) > 365 OR DATE_EMPRUNT IS NULL ";
+
+            OleDbCommand cmd = new OleDbCommand(request, dbCon);
+            cmd.ExecuteNonQuery();
+
+            OleDbDataReader reader = cmd.ExecuteReader();
+            while (reader.Read())
+            {
+                test.Add(reader.GetInt32(0));
+            }
+            if (adminAcc.listeAlbNonEmprUnAn.Count>0)
+            {
+                for (int i = 0; i < adminAcc.listeAlbNonEmprUnAn.Count; i++)
+                {
+                    if (!test[i].Equals(adminAcc.listeAlbNonEmprUnAn[i]))
+                    {
+                        same = false;
+                    }
+                }
+            }
+            Assert.IsTrue(same);
+        }
+    }
+}

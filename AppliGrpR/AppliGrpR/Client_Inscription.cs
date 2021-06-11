ff2877be439af3913397dd5ca206617af591b816
Pierre Laporte <pierre.laporte.1@u-bordeaux.fr>
2021-06-11 13:57:43 +0200

    Documentation du code

--- AppliGrpR/AppliGrpR/Client_Inscription.cs
+++ AppliGrpR/AppliGrpR/Client_Inscription.cs
@@ -33,7 +33,6 @@ namespace AppliGrpR
         /// Permet de vérifier si les paramètres pour l'inscription sont valides
         /// </summary>
         /// <returns>TRUE si valide, FALSE sinon</returns>
-        /// 
         public bool InscriptionValide()
         {
             bool valide = true;
